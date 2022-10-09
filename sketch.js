c3a1600ec2444cf965b963280f1ef8ed713b7a4e
jdcruzc1 <jdcruzc1@gmail.com>
2022-10-09 01:21:42 -0500

    Primer Commit

--- sketch.js
+++ sketch.js
@@ -1,14 +1,164 @@
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(1920, 1080);
+    background(137, 37, 15);
   }
   
   function draw() {
-    background(220);
-    noStroke();
-    fill(0, 255, 0);
+    fill(255, 73, 0);
+    ellipse(300, 300, 150, 150);
+    fill(255, 135, 0);
     ellipse(200, 200, 100, 100);
-    fill(0);
-    textSize(40);
-    textAlign(CENTER, CENTER);
-    text(':-)', 200, 200);
+    fill(255, 213, 0);
+    ellipse (130, 130, 60, 60);
+    fill(251, 255, 0);
+    ellipse (80, 80, 30, 30);
+    fill(255, 213, 0);
+
+    fill(255, 0, 0); //bolas chikitas der 1 
+    ellipse(360, 200, 30, 30);
+
+    fill(255, 0, 0); //bolas chikitas der 2
+    ellipse(345, 150, 20, 20);
+
+    fill(255, 0, 0); //bolas chikitas der 3
+    ellipse(330, 100, 10, 10);
+
+    fill(255, 0, 0); //bolas chikitas der 4
+    ellipse(315, 50, 5, 5);
+
+    fill(255, 0, 0); //bolas chikitas izq 1
+    ellipse(185, 340, 30, 30);
+
+    fill(255, 0, 0); //bolas chikitas izq 2 
+    ellipse(140, 330, 20, 20);
+
+    fill(255, 0, 0); //bolas chikitas izq 3 
+    ellipse(95, 320, 10, 10);
+
+    fill(255, 0, 0); //bolas chikitas izq 4
+    ellipse(50, 310, 5, 5);
+
+    fill(0, 0, 0); // Espacio
+    ellipse (950, 100, 800, 700);
+
+    fill(131, 227, 225); // estrellas
+    ellipse (950, 100, 5, 5);
+
+    fill(131, 227, 225); // estrellas
+    ellipse (800, 300, 5, 5);
+
+    fill(131, 227, 225); // estrellas
+    ellipse (700, 200, 5, 5);
+
+    fill(131, 227, 225); // estrellas
+    ellipse (750, 50, 5, 5);
+
+    fill(131, 227, 225); // estrellas
+    ellipse (1050, 150, 5, 5);
+
+    fill(131, 227, 225); // estrellas
+    ellipse (1150, 30, 5, 5);
+
+    fill(131, 227, 225); // estrellas
+    ellipse (1200, 150, 5, 5);
+
+    fill(131, 227, 225); // estrellas
+    ellipse (1150, 250, 5, 5);
+
+    fill(131, 227, 225); // estrellas
+    ellipse (600, 100, 5, 5);
+
+    fill(131, 227, 225); // estrellas
+    ellipse (800, 150, 5, 5);
+
+    fill(21, 166, 250); // CAPSULA
+    ellipse (950, 280, 200, 200);
+
+    fill(13, 174, 27); // CABEZA
+    ellipse (950, 265, 70, 100);
+
+    fill(126, 12, 10); // OJOS
+    ellipse (940, 250, 15, 30);
+
+    fill(126, 12, 10); // OJOS
+    ellipse (960, 250, 15, 30);
+
+    fill(126, 12, 10); // boca
+    ellipse (950, 290, 20, 30);
+
+    fill(17, 19, 109); // NAVE
+    ellipse (950, 400, 600, 200);
+
+    fill(69, 24, 148); // luz capsula 1
+    ellipse (1220, 400, 40, 40);
+    
+    fill(23, 115, 38); // luz capsula 2
+    ellipse (1160, 430, 40, 40);
+
+    fill(69, 24, 148); // luz capsula 3
+    ellipse (1100, 450, 40, 40);
+    
+    fill(69, 24, 148); // luz capsula 4
+    ellipse (680, 400, 40, 40);
+    
+    fill(23, 115, 38); // luz capsula 5
+    ellipse (740, 430, 40, 40);
+
+    fill(69, 24, 148); // luz capsula 6
+    ellipse (800, 450, 40, 40);
+
+    fill(255, 247, 0); // orificio
+    ellipse (950, 470, 200, 50);
+
+    fill(255, 0, 0); // laser
+    rect (950, 445, 10, 1080);
+
+    fill(137, 37, 15); //orbita 1 planeta 1
+    ellipse(1650, 220, 600, 300);
+
+    fill(137, 37, 15); //orbita 2 planeta 2
+    ellipse(1650, 220, 590, 290);
+
+    fill(62, 0, 255); //planeta 1
+    ellipse(1680, 220, 400, 400);
+
+    fill(166, 141, 246); //planeta 2
+    ellipse(1530, 340, 250, 250);
+
+    fill(21, 166, 250); // CAPSULA pequeña 1
+    ellipse (1200, 100, 50, 50);
+
+    fill(17, 19, 109); // NAVE pequeña 1
+    ellipse (1200, 130, 100, 50); 
+
+    fill(21, 166, 250); // CAPSULA pequeña 2
+    ellipse (720, 100, 50, 50);
+
+    fill(17, 19, 109); // NAVE pequeña 2
+    ellipse (720, 130, 100, 50); 
+
+    fill(21, 166, 250); // CAPSULA pequeña 3
+    ellipse (620, 30, 25, 25);
+
+    fill(17, 19, 109); // NAVE pequeña 3
+    ellipse (620, 45, 50, 25); 
+
+    fill(21, 166, 250); // CAPSULA pequeña 4
+    ellipse (1280, 30, 25, 25);
+
+    fill(17, 19, 109); // NAVE pequeña 4
+    ellipse (1280, 45, 50, 25); 
+
+    fill(137, 131, 131); // edificio 1
+    rect (200, 600, 500, 1080);
+
+    fill(195, 192, 192); // edificio 2
+    rect (600, 800, 300, 1080);
+
+    fill(137, 131, 131); // edificio 3
+    rect (1100, 600, 500, 1080);
+
+    fill(195, 192, 192); // edificio 4
+    rect (1500, 800, 300, 1080);
   }
+ 
